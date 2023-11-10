a66949c5f4b6d9b53475eddae1e2ed00b161eb9d
Sina Nakhaei <36819672+sina-nakhaei@users.noreply.github.com>
2023-11-10 22:38:51 +0330

    feat: <LoadingButtonElevation>

---
+++ app/src/main/java/com/sinanakhaei/button/ui/theme/loadingbutton/LoadingButtonElevation.kt
@@ -0,0 +1,9 @@
+package com.sinanakhaei.button.ui.theme.loadingbutton
+
+import androidx.compose.ui.unit.Dp
+
+data class LoadingButtonElevation(
+    val defaultElevation: Dp,
+    val pressedElevation: Dp,
+    val disabledElevation: Dp
+)
