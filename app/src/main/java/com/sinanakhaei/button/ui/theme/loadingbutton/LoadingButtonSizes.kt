af2965d9f2cf1838b6058a0f77e00ed9c5ee5ee1
Sina Nakhaei <36819672+sina-nakhaei@users.noreply.github.com>
2023-11-10 22:38:36 +0330

    feat: <LoadingButtonSizes>

---
+++ app/src/main/java/com/sinanakhaei/button/ui/theme/loadingbutton/LoadingButtonSizes.kt
@@ -0,0 +1,9 @@
+package com.sinanakhaei.button.ui.theme.loadingbutton
+
+import androidx.compose.ui.unit.Dp
+
+data class LoadingButtonSizes(
+    val width: Dp,
+    val height: Dp,
+    val progressWidth: Dp
+)
